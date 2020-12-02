cc75a03c742d9fa342324f5d406f075204978445
edwin-mrdigital <57614380+edwin-mrdigital@users.noreply.github.com>
2020-12-02 10:50:28 +0530

    Add sticky header code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function(){
 
-	console.log('jQuery');
+	//STICKY HEADER
+	jQuery(window).scroll(function() {    
+	    var scroll = jQuery(window).scrollTop();
+	    if (scroll >= 1) {
+	        jQuery(".site-header").addClass("scrolled");
+	    } else {
+	        jQuery(".site-header").removeClass("scrolled");
+	    }
+	});
 
 }); //Script End
