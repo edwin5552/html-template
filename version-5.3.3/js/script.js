ad1b37d793aad116a4b10e0cb4afdc07ae634f50
Edwin <edwin@encome.in>
2024-07-16 10:00:44 +0530

    Update script.js

--- version-5.3.3/js/script.js
+++ version-5.3.3/js/script.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
+jQuery(document).ready(function () {
+
+	//CONSOLE LOG
 	console.log('jQuery Enable');
-});
+
+	//STICKY HEADER
+	jQuery(window).scroll(function () {
+		var scroll = jQuery(window).scrollTop();
+		if (scroll >= 1) {
+			jQuery(".site-header").addClass("scrolled");
+		} else {
+			jQuery(".site-header").removeClass("scrolled");
+		}
+	});
+
+}); //Script End
